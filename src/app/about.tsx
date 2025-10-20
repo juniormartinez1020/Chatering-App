17465d151cde7f279685701de5321926e4eecdc5
juniormartinez1020 <juniormperalta33@gmail.com>
2025-10-19 20:18:33 -0400

    configuracion expo router

---
+++ src/app/about.tsx
@@ -0,0 +1,11 @@
+
+
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-3xl'>about...</Text>
+        </View>
+    )
+}
