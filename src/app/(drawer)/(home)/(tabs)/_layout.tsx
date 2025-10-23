8f7b6c83a23b074f45ccb50e376143c46aa60793
juniormartinez1020 <juniormperalta33@gmail.com>
2025-10-23 18:55:24 -0400

    agregar mas tablas

--- src/app/(drawer)/(home)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(home)/(tabs)/_layout.tsx
@@ -1,6 +1,11 @@
+import { Tabs } from 'expo-router';
 import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabsLayout() {
+
+  const isGlass = true
+
+  if (isGlass) {   
     return (
         <NativeTabs>
         <NativeTabs.Trigger name="chats">
@@ -11,10 +16,14 @@ export default function TabsLayout() {
           <Icon sf="gear" drawable="custom_settings_drawable" />
           <Label>Settings</Label>
         </NativeTabs.Trigger>
-
+  
         <NativeTabs.Trigger name='search' role='search'>
           <Label>search</Label>
         </NativeTabs.Trigger>
       </NativeTabs>
     )
+  }
+
+  return <Tabs />
+
 }
