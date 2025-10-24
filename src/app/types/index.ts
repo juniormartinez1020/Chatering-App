aebcf237c964de864e60bdb5249c53dc76be914f
juniormartinez1020 <juniormperalta33@gmail.com>
2025-10-24 18:20:02 -0400

    agregando datos ficticios y sus tipos

---
+++ src/app/types/index.ts
@@ -0,0 +1,25 @@
+export type Channel = {
+    id: string
+    name: string
+    lastMessage: Message
+    avatar: string
+}
+
+
+export type Message = {
+    id: string
+    created_at: string
+    content: string
+    sender?: User 
+    image?: string
+}
+
+export type User = {
+    id: string
+    first_name: string
+    last_name: string
+    full_name: string
+    avatar_url?: string | null
+    created_at: string
+    updated_at: string
+}
